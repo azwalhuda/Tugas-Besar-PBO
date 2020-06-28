d84530becc7ac36504f38cd73f8e594720a04c8a
azwalhuda <alifzaw25@gmail.com>
2020-06-28 09:01:50 +0700

    Add files via upload

---
+++ TUBES PBO/src/com/azwalhuda/bengkel/Koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.azwalhuda.bengkel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author LAPTOP FM
+ */
+public class Koneksi {
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/server_bengkel";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+            
+        }catch (SQLException e){
+            System.out.println("Gagal Konek DB" +e.getMessage());
+        }
+        return MySQLConfig;
+    }
+}
